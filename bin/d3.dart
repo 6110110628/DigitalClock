e09b5091d0b89f30a50c7fd75bd97aa3e399e57d
6110110628 <6110110628@psu.ac.th>
2021-07-05 19:36:40 +0700

    Round2

--- bin/d3.dart
+++ bin/d3.dart
@@ -1,5 +1,18 @@
-void main(List<String> arguments) {
-  List<String> status = ['on 18.0', 'set', 'inc', 'set', 'inc', 'inc', 'set'];
+class Idle {}
 
-  print(status);
+class SetHours {}
+
+class SetMins {}
+
+void main(List<String> arguments) {
+  List<String> input = ['on 18.0', 'set', 'inc', 'set', 'inc', 'inc', 'set'];
+  var hour = 0;
+  var min = 0;
+  var state = 0;
+
+  for (var input in input) {
+    print(input);
+  }
+
+  //print(input);
 }
